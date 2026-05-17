8ec7981fbf89ef240e97e7d9b669a1d853424e3b
alocc911 <alocc247@gmail.com>
2026-05-17 12:00:50 -0500

    Allow tapping cutscene backdrop to continue

--- scripts/CutsceneLibrary.gd
+++ scripts/CutsceneLibrary.gd
@@ -5,7 +5,7 @@ static func get_cutscene_definition(cutscene_id: String) -> Dictionary:
 		"post_tutorial_intro":
 			return {
 				"id": "post_tutorial_intro",
-				"background": "res://assets/boss/boss_head_face.jpg",
+				"background": LevelConfig.RESORT_SAND_TILE_TEXTURE_PATH,
 				"player_sprite": "res://assets/ui/icons/icon_seed.png",
 				"other_sprite": "res://assets/ui/icons/icon_gold.png",
 				"player_dialogue": "Player dialogue",
